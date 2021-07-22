5054f8e5e56030d57ceb875eb2dbac954262b648
MatteaAnn Swain <MMSwain@tql.com>
2021-07-22 12:21:59 -0400

    fixed upload bug

--- src/app/upload/upload.component.ts
+++ src/app/upload/upload.component.ts
@@ -26,7 +26,7 @@ export class UploadComponent implements OnInit {
     let fileToUpload = <File>files[0];
     const formData = new FormData();
     formData.append('file', fileToUpload, fileToUpload.name);
-    this.http.post('https://localhost:23844/api/upload', formData, { reportProgress: true, observe: 'events' })
+    this.http.post('http://localhost:23844/api/ImportDatas/upload', formData, { reportProgress: true, observe: 'events' })
       .subscribe(event => {
         if (event.type === HttpEventType.UploadProgress)
           this.progress = Math.round(100 * event.loaded / event.total!);
